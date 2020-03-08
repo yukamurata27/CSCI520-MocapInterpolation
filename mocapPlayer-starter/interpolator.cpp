a6e9a2620511a1fd462a9de4d0189b1d3f11e8fe
yukamurata27 <yukamurata27@gmail.com>
2020-03-08 14:48:17 -0700

    Implement Bezier quaternion interpolation

--- mocapPlayer-starter/interpolator.cpp
+++ mocapPlayer-starter/interpolator.cpp
@@ -188,7 +188,98 @@ void Interpolator::LinearInterpolationQuaternion(Motion * pInputMotion, Motion *
 
 void Interpolator::BezierInterpolationQuaternion(Motion * pInputMotion, Motion * pOutputMotion, int N)
 {
-  // students should implement this
+  int inputLength = pInputMotion->GetNumFrames(); // frames are indexed 0, ..., inputLength-1
+
+  int startKeyframe = 0;
+  while (startKeyframe + N + 1 < inputLength)
+  {
+    int endKeyframe = startKeyframe + N + 1;
+
+    Posture * startPosture = pInputMotion->GetPosture(startKeyframe);
+    Posture * endPosture = pInputMotion->GetPosture(endKeyframe);
+
+    // copy start and end keyframe
+    pOutputMotion->SetPosture(startKeyframe, *startPosture);
+    pOutputMotion->SetPosture(endKeyframe, *endPosture);
+
+    // interpolate in between
+    for(int frame=1; frame<=N; frame++)
+    {
+      Posture interpolatedPosture;
+      double t = 1.0 * frame / (N+1);
+
+      // interpolate root position
+      interpolatedPosture.root_pos = startPosture->root_pos * (1-t) + endPosture->root_pos * t;
+
+      // interpolate bone rotations
+      for (int bone = 0; bone < MAX_BONES_IN_ASF_FILE; bone++) {
+        Quaternion<double> startQ, endQ, a, b, interpQ;
+        
+        // convert Euler to quaternion
+        double arrRotS[3] = {startPosture->bone_rotation[bone].x(), startPosture->bone_rotation[bone].y(), startPosture->bone_rotation[bone].z()};
+        Euler2Quaternion(arrRotS, startQ);
+        double arrRotE[3] = {endPosture->bone_rotation[bone].x(), endPosture->bone_rotation[bone].y(), endPosture->bone_rotation[bone].z()};
+        Euler2Quaternion(arrRotE, endQ);
+
+        // Get a (second control point)
+        if (startKeyframe == 0) {
+          Posture * q3Posture = pInputMotion->GetPosture(endKeyframe + N + 1);
+          double arrRot3[3] = {q3Posture->bone_rotation[bone].x(), q3Posture->bone_rotation[bone].y(), q3Posture->bone_rotation[bone].z()};
+          Quaternion<double> q3;
+          Euler2Quaternion(arrRot3, q3);
+          Quaternion<double> intermediate = Slerp(2.0, q3, endQ);
+          a = Slerp(1.0/3.0, startQ, intermediate);
+        } else {
+          Posture * prevPosture = pInputMotion->GetPosture(startKeyframe - (N + 1));
+          double arrRotPrev[3] = {prevPosture->bone_rotation[bone].x(), prevPosture->bone_rotation[bone].y(), prevPosture->bone_rotation[bone].z()};
+          Quaternion<double> prev;
+          Euler2Quaternion(arrRotPrev, prev);
+          Quaternion<double> intermediate = Slerp(2.0, prev, startQ);
+          Quaternion<double> aa = Slerp(0.5, intermediate, endQ);
+
+          a = Slerp(1.0/3.0, startQ, aa);
+        }
+        
+        // Get b (third control point)
+        if (endKeyframe + N + 1 > inputLength) {
+          Posture * PrevPrevPosture = pInputMotion->GetPosture(startKeyframe - (N + 1));
+          double arrRotPrevPrev[3] = {PrevPrevPosture->bone_rotation[bone].x(), PrevPrevPosture->bone_rotation[bone].y(), PrevPrevPosture->bone_rotation[bone].z()};
+          Quaternion<double> qPrevPrev;
+          Euler2Quaternion(arrRotPrevPrev, qPrevPrev);
+          Quaternion<double> intermediate = Slerp(2.0, qPrevPrev, startQ);
+          b = Slerp(1.0/3.0, endQ, intermediate);
+        } else {
+          Posture * nextPosture = pInputMotion->GetPosture(endKeyframe + N + 1);
+          double arrRotNext[3] = {nextPosture->bone_rotation[bone].x(), nextPosture->bone_rotation[bone].y(), nextPosture->bone_rotation[bone].z()};
+          Quaternion<double> next;
+          Euler2Quaternion(arrRotNext, next);
+          Quaternion<double> intermediate = Slerp(2.0, startQ, endQ);
+          Quaternion<double> aa = Slerp(0.5, intermediate, next);
+          b = Slerp(-1.0/3.0, endQ, aa);
+        }
+        
+        // interpolate
+        interpQ = DeCasteljauQuaternion(t, startQ, a, b, endQ);
+
+        // If SLERP is not applicable, use linear interpolation
+        if (isnan(interpQ.Gets())) {
+          interpolatedPosture.bone_rotation[bone] = startPosture->bone_rotation[bone] * (1-t) + endPosture->bone_rotation[bone] * t;
+        } else {
+          // convert quaternion to Euler
+          double angles[3];
+          Quaternion2Euler(interpQ, angles);
+          interpolatedPosture.bone_rotation[bone] = angles;
+        }
+      }
+
+      pOutputMotion->SetPosture(startKeyframe + frame, interpolatedPosture);
+    }
+
+    startKeyframe = endKeyframe;
+  }
+
+  for(int frame=startKeyframe+1; frame<inputLength; frame++)
+    pOutputMotion->SetPosture(frame, *(pInputMotion->GetPosture(frame)));
 }
 
 void Interpolator::Euler2Quaternion(double angles[3], Quaternion<double> & q) 
@@ -235,8 +326,14 @@ vector Interpolator::DeCasteljauEuler(double t, vector p0, vector p1, vector p2,
 
 Quaternion<double> Interpolator::DeCasteljauQuaternion(double t, Quaternion<double> p0, Quaternion<double> p1, Quaternion<double> p2, Quaternion<double> p3)
 {
-  // students should implement this
-  Quaternion<double> result;
-  return result;
+  Quaternion<double> q0, q1, q2, r0, r1;
+
+  q0 = Slerp(t, p0, p1);
+  q1 = Slerp(t, p1, p2);
+  q2 = Slerp(t, p2, p3);
+  r0 = Slerp(t, q0, q1);
+  r1 = Slerp(t, q1, q2);
+
+  return Slerp(t, r0, r1);
 }
 
