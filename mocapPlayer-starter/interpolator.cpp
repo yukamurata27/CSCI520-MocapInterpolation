1f76fba88d990060baccae7f1aa4cf308cb34039
yukamurata27 <yukamurata27@gmail.com>
2020-03-08 00:01:00 -0800

    Implement linear interpolation with quaternion

--- mocapPlayer-starter/interpolator.cpp
+++ mocapPlayer-starter/interpolator.cpp
@@ -6,6 +6,9 @@
 #include "interpolator.h"
 #include "types.h"
 
+#include "transform.h"
+#include <math.h>
+
 Interpolator::Interpolator()
 {
   //Set default interpolation type
@@ -103,17 +106,84 @@ void Interpolator::Rotation2Euler(double R[9], double angles[3])
 
 void Interpolator::Euler2Rotation(double angles[3], double R[9])
 {
-  // students should implement this
+  double rX[4][4], rY[4][4], rZ[4][4], rZYX[4][4], rYX[4][4];
+
+  // Set each rotation matrix
+  rotationX(rX, angles[0]);
+  rotationY(rY, angles[1]);
+  rotationZ(rZ, angles[2]);
+
+  // Rotation matrix multiplications
+  matrix_mult(rY, rX, rYX);
+  matrix_mult(rZ, rYX, rZYX);
+
+  // Feed the result back to R
+  R[0] = rZYX[0][0]; R[1] = rZYX[0][1]; R[2] = rZYX[0][2];
+  R[3] = rZYX[1][0]; R[4] = rZYX[1][1]; R[5] = rZYX[1][2];
+  R[6] = rZYX[2][0]; R[7] = rZYX[2][1]; R[8] = rZYX[2][2];
 }
 
 void Interpolator::BezierInterpolationEuler(Motion * pInputMotion, Motion * pOutputMotion, int N)
 {
-  // students should implement this
 }
 
 void Interpolator::LinearInterpolationQuaternion(Motion * pInputMotion, Motion * pOutputMotion, int N)
 {
-  // students should implement this
+  int inputLength = pInputMotion->GetNumFrames(); // frames are indexed 0, ..., inputLength-1
+
+  int startKeyframe = 0;
+  while (startKeyframe + N + 1 < inputLength)
+  {
+    int endKeyframe = startKeyframe + N + 1;
+
+    Posture * startPosture = pInputMotion->GetPosture(startKeyframe);
+    Posture * endPosture = pInputMotion->GetPosture(endKeyframe);
+
+    // copy start and end keyframe
+    pOutputMotion->SetPosture(startKeyframe, *startPosture);
+    pOutputMotion->SetPosture(endKeyframe, *endPosture);
+
+    // interpolate in between
+    for(int frame=1; frame<=N; frame++)
+    {
+      Posture interpolatedPosture;
+      double t = 1.0 * frame / (N+1);
+
+      // interpolate root position
+      interpolatedPosture.root_pos = startPosture->root_pos * (1-t) + endPosture->root_pos * t;
+
+      // interpolate bone rotations
+      for (int bone = 0; bone < MAX_BONES_IN_ASF_FILE; bone++) {
+        Quaternion<double> startQ, endQ, interpQ;
+        
+        // convert Euler to quaternion
+        double arrRotS[3] = {startPosture->bone_rotation[bone].x(), startPosture->bone_rotation[bone].y(), startPosture->bone_rotation[bone].z()};
+        Euler2Quaternion(arrRotS, startQ);
+        double arrRotE[3] = {endPosture->bone_rotation[bone].x(), endPosture->bone_rotation[bone].y(), endPosture->bone_rotation[bone].z()};
+        Euler2Quaternion(arrRotE, endQ);
+        
+        // interpolate
+        interpQ = Slerp(t, startQ, endQ);
+
+        // If SLERP is not applicable, use linear interpolation
+        if (isnan(interpQ.Gets())) {
+          interpolatedPosture.bone_rotation[bone] = startPosture->bone_rotation[bone] * (1-t) + endPosture->bone_rotation[bone] * t;
+        } else {
+          // convert quaternion to Euler
+          double angles[3];
+          Quaternion2Euler(interpQ, angles);
+          interpolatedPosture.bone_rotation[bone] = angles;
+        }
+      }
+
+      pOutputMotion->SetPosture(startKeyframe + frame, interpolatedPosture);
+    }
+
+    startKeyframe = endKeyframe;
+  }
+
+  for(int frame=startKeyframe+1; frame<inputLength; frame++)
+    pOutputMotion->SetPosture(frame, *(pInputMotion->GetPosture(frame)));
 }
 
 void Interpolator::BezierInterpolationQuaternion(Motion * pInputMotion, Motion * pOutputMotion, int N)
@@ -123,19 +193,30 @@ void Interpolator::BezierInterpolationQuaternion(Motion * pInputMotion, Motion *
 
 void Interpolator::Euler2Quaternion(double angles[3], Quaternion<double> & q) 
 {
-  // students should implement this
+  double R[9];
+  Euler2Rotation(angles, R);
+  q = Quaternion<double>::Matrix2Quaternion(R);
 }
 
 void Interpolator::Quaternion2Euler(Quaternion<double> & q, double angles[3]) 
 {
-  // students should implement this
+  double R[9];
+  q.Quaternion2Matrix(R);
+  Rotation2Euler(R, angles);
 }
-
+#include <iostream>
 Quaternion<double> Interpolator::Slerp(double t, Quaternion<double> & qStart, Quaternion<double> & qEnd_)
 {
-  // students should implement this
-  Quaternion<double> result;
-  return result;
+  Quaternion<double> q1, q2;
+  qStart.Normalize();
+  qEnd_.Normalize();
+  double theta = acos(
+                  qStart.Gets() * qEnd_.Gets() +
+                  qStart.Getx() * qEnd_.Getx() +
+                  qStart.Gety() * qEnd_.Gety() +
+                  qStart.Getz() * qEnd_.Getz() );
+
+  return qStart*(sin((1.f-t)*theta)/sin(theta)) + qEnd_*(sin(t*theta)/sin(theta));
 }
 
 Quaternion<double> Interpolator::Double(Quaternion<double> p, Quaternion<double> q)
